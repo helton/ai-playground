a8e17d30b29ce01687dce810c9ca5b1a47c6fd97
Helton Carlos de Souza <heltoncarlossouza@gmail.com>
2023-11-02 18:14:25 -0300

    feature: add intro-to-vector-db project

---
+++ intro-to-vector-db/main.py
@@ -0,0 +1,33 @@
+import os
+
+from langchain.chains import RetrievalQA
+from langchain.document_loaders import TextLoader
+from langchain.llms.openai import OpenAI
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import Pinecone
+import pinecone
+
+pinecone.init(api_key=os.environ['PINECONE_API_KEY'], environment=os.environ['PINECONE_ENVIRONMENT'])
+
+if __name__ == '__main__':
+    loader = TextLoader('docs/blog.txt', encoding='utf8')
+    document = loader.load()
+    print(document)
+
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    texts = text_splitter.split_documents(document)
+    print(len(texts))
+
+    embeddings = OpenAIEmbeddings(openai_api_key=os.environ['OPENAI_API_KEY'])
+    doc_search = Pinecone.from_documents(texts, embeddings, index_name=os.environ['PINECONE_INDEX_NAME'])
+
+    qa = RetrievalQA.from_chain_type(
+        llm=OpenAI(),
+        chain_type="stuff",
+        retriever=doc_search.as_retriever(),
+        return_source_documents=True
+    )
+    query = "What is a Vector DB? Give me a 15 word answer for a beginner."
+    result = qa({"query": query})
+    print(result)
