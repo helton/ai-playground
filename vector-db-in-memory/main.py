ec2713d863cbd8eb9c0ced6245aab43a3d71da43
Helton Carlos de Souza <heltoncarlossouza@gmail.com>
2023-11-02 18:15:06 -0300

    feature: add vector-db-in-memory project

---
+++ vector-db-in-memory/main.py
@@ -0,0 +1,26 @@
+from langchain.document_loaders import PyPDFLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chains import RetrievalQA
+from langchain.llms import OpenAI
+
+if __name__ == '__main__':
+    pdf_path = "docs/2210.03629.pdf"
+    loader = PyPDFLoader(file_path=pdf_path)
+    documents = loader.load()
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=30, separator="\n")
+    docs = text_splitter.split_documents(documents)
+
+    embeddings = OpenAIEmbeddings()
+    vector_store = FAISS.from_documents(docs, embeddings)
+    vector_store.save_local("faiss/faiss_index_react")
+
+    new_vector_store = FAISS.load_local("faiss/faiss_index_react", embeddings)
+    qa = RetrievalQA.from_chain_type(
+        llm=OpenAI(),
+        chain_type="stuff",
+        retriever=new_vector_store.as_retriever()
+    )
+    result = qa.run("Give me the gist of ReAct in 3 sentences")
+    print(result)
